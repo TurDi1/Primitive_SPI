9ce108a780e99644a143b0ad3e5f801069331c5f
TurDi1 <90939428+TurDi1@users.noreply.github.com>
2026-07-24 00:33:20 +0300

    upd tb for ppc

--- SPI_EXAMPLE/source_files/tb/ppc_tb.sv
+++ SPI_EXAMPLE/source_files/tb/ppc_tb.sv
@@ -4,7 +4,7 @@ module ppc_tb ();
 //==================================
 //           PARAMETERS
 //==================================
-parameter CLK_PULSE_WIDTH = 10ns;   // Freq - 5 MHz
+parameter CLK_PULSE_WIDTH = 10ns;   // Freq - 50 MHz
 
 int unsigned rst_time;              // Time variable for reset
 
@@ -20,6 +20,8 @@ reg    [1:0]            spibr_reg;
 reg    [2:0]            spicr_reg;
 
 wire                    baud_rate_wire;
+reg    [1:0]            spibr_for_bd;
+reg    [2:0]            spicr_for_ppc;
 //wire                    sample_clk_wire;
 //wire                    shift_clk_wire;
 
@@ -42,7 +44,7 @@ BAUD_RATE_GENERATOR baud_rate_gen (
     .fsm_rstn   ( ~fsm_rst_reg ),
     .clk        ( sys_clk_reg ),
     .clk_en     ( clk_en_reg ),
-    .spibr      ( spibr_reg ),
+    .spibr      ( spibr_for_bd ),
     .baud_rate  ( baud_rate_wire )
 );
 
@@ -51,7 +53,7 @@ PPC ppc (
 	.fsm_rstn   ( ~fsm_rst_reg ),
 	.clk        ( sys_clk_reg ),
 	.baud_clk   ( baud_rate_wire ),
-	.spicr      ( spicr_reg ),
+	.spicr      ( spicr_for_ppc ),
 	.sample_clk (  ),
 	.shift_clk  (  )
 );
@@ -80,7 +82,7 @@ end
 endtask
 
 // Task for change baud rate settings
-task change_settings;
+task change_bd_settings;
 input [1:0] spibr_value;
 
 begin
@@ -90,11 +92,67 @@ begin
     $display("--------------------------------------------------------");
     @(posedge sys_clk_reg);
     clk_en_reg = 0;
-    spibr_reg = spibr_value;
+    spibr_for_bd = spibr_value;
     
-    $display("[TB INFO]  VALUE IN HEX -> %h", spibr_reg);
+    $display("[TB INFO]  VALUE IN HEX -> %h", spibr_for_bd);
     @(posedge sys_clk_reg);
     clk_en_reg = 1;
+end
+endtask
+
+// Task for change ppc module settings
+task change_ppc_settings;
+input [2:0] spicr_value;
+
+begin
+    $display("--------------------------------------------------------");
+    $display("[TB INFO]  CHANGE SETTING OF PPC THROUGH spicr BUS!");
+    $display("TIME:  %0t", $realtime);
+    $display("--------------------------------------------------------");
+    @(posedge sys_clk_reg);
+    spicr_for_ppc = spicr_value;
+
+    $display("[TB INFO]  VALUE IN HEX -> %h", spicr_for_ppc);
+    @(posedge sys_clk_reg);
+end
+endtask
+
+// Task for check sample_ and shift_ signals of ppc
+task chk_ppc;
+begin
+    $display("--------------------------------------------------------");
+    $display("[TB INFO]  CHECK SAMPLE_ & SHIFT_ OUTPUTS OF PPC");
+    $display("TIME:  %0t", $realtime);
+    $display("--------------------------------------------------------");
+
+    repeat (5)
+    begin
+        if (ppc.spicr == 3'b000) // CPOL bit 1 = 0
+        begin
+            @(posedge baud_rate_wire);
+            if (ppc.sample_clk && (!ppc.shift_clk))
+            begin
+               $display("[TB INFO]  SAMPLE AND SHIFT CLK ENABLES ARE CORRECT FOR CPOL = 0 AT RISING EDGE OF BAUD CLK");
+               $display("SAMPPLE VALUE = %0h, SHIFT VALUE = %0h", ppc.sample_clk, ppc.shift_clk);
+               $display("TIME:  %0t", $realtime); 
+               $display("--------------------------------------------------------");
+            end
+            else
+            begin
+                $error("[TB ERROR]  SAMPLE AND SHIFT CLK ENABLES ARE NOT CORRECT AT RISING EDGE OF BAUD CLK");
+                $finish;
+            end
+            // CODE NOT COMPELETED!
+        end
+        
+        // else
+        // begin
+            
+        // end
+
+        //@(negedge baud_rate_wire);
+        
+    end
 end
 endtask
 
@@ -116,20 +174,12 @@ begin
 
     system_reset();
         
-    spibr_reg = 2'b00;  // CLK/2 - BAUD RATE
-    spicr_reg = 3'b000; // Mode 0;
-    change_settings(spibr_reg);
-
-    @(posedge ppc.sample_clk);
-    @(posedge ppc.sample_clk);
-    @(posedge ppc.sample_clk);
-    @(posedge ppc.sample_clk);
-    @(posedge ppc.sample_clk);
-    @(posedge ppc.sample_clk);
-    @(posedge ppc.sample_clk);
-    @(posedge ppc.sample_clk);
-    @(posedge ppc.sample_clk);
-    @(posedge ppc.sample_clk);
-    @(posedge ppc.sample_clk);
+    spibr_reg = 2'b01;             // BAUD RATE - CLK/4
+    spicr_reg = 3'b000;            // Mode 0;
+    change_bd_settings(spibr_reg);
+    change_ppc_settings(spicr_reg);
+    
+    chk_ppc();
+    $finish;
 end
 endmodule
